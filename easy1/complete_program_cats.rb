386970850ee3bbaa67b75e8b098f43cd10283fbe
Hyunjoo Jun <hjj102088@gmail.com>
2022-10-30 14:57:57 -0400

    Complete Program

---
+++ easy1/complete_program_cats.rb
@@ -0,0 +1,20 @@
+class Pet
+  def initialize(name, age, color)
+    @name = name
+    @age = age
+    @color = color
+  end
+end
+
+class Cat < Pet
+  def to_s
+    "My cat #{@name} is #{@age} years old and has #{@color} fur."
+  end
+end
+
+pudding = Cat.new('Pudding', 7, 'black and white')
+butterscotch = Cat.new('Butterscotch', 10, 'tan and white')
+puts pudding, butterscotch
+
+# My cat Pudding is 7 years old and has black and white fur.
+# My cat Butterscotch is 10 years old and has tan and white fur.
