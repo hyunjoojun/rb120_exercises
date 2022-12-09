57b2124c8d4cb2048e57a5cda77dd9a343c1d03a
Hyunjoo Jun <hjj102088@gmail.com>
2022-12-09 14:32:45 -0500

    Review

--- medium1/number_guesser1.rb
+++ medium1/number_guesser1.rb
@@ -1,56 +1,68 @@
 class GuessingGame
-  attr_accessor :number_of_guesses
   MAX_GUESSES = 7
   RANGE = 1..100
 
   def initialize
-    @winning_number = RANGE.to_a.sample
     @number_of_guesses = MAX_GUESSES
-  end
-
-  def display_number_of_guesses
-    puts "You have #{@number_of_guesses} guesses remaining."
-  end
-
-  def ask_user_to_guess
-    guessed_num = 0
-    loop do
-      puts 'Enter a number between 1 and 100:'
-      guessed_num = gets.chomp.to_i
-      break if RANGE.to_a.include?(guessed_num)
-
-      puts 'Invalid guess.'
-    end
-    guessed_num
-  end
-
-  def give_hint(guessed_num)
-    if guessed_num > @winning_number
-      puts 'Your guess is too high.'
-    elsif @winning_number > guessed_num
-      puts 'Your guess is too low.'
-    else
-      you_won
-    end
-  end
-
-  def you_won
-    puts "That's the number! You won!"
-  end
-
-  def you_lost
-    puts 'You have no more guesses. You lost!'
+    @winning_num = RANGE.to_a.sample
+    @guessed_num = nil
   end
 
   def play
     loop do
       display_number_of_guesses
-      guessed_num = ask_user_to_guess
-      give_hint(guessed_num)
-      @number_of_guesses -= 1
-      break if @number_of_guesses.zero? || guessed_num == @winning_number
+      guess_number
+      evaluate_number
+      break if game_over? || won?
     end
-    you_lost if @number_of_guesses.zero?
+    puts you_lost if game_over? && !won?
+  end
+
+  private
+
+  attr_reader :number_of_guesses, :guessed_num, :winning_num
+
+  def you_won
+    "That's the number! You won!"
+  end
+
+  def you_lost
+    'You have no more guesses. You lost!'
+  end
+
+  def game_over?
+    number_of_guesses.zero?
+  end
+
+  def won?
+    guessed_num == winning_num
+  end
+
+  def display_number_of_guesses
+    puts "You have #{number_of_guesses} guesses remaining."
+  end
+
+  def guess_number
+    loop do
+      puts 'Enter a number between 1 and 100:'
+      num = gets.chomp.to_i
+      if RANGE.include?(num)
+        @guessed_num = num
+        @number_of_guesses -= 1
+        break
+      end
+      puts 'Invalid guess.'
+    end
+  end
+
+  def evaluate_number
+    if guessed_num > winning_num
+      puts 'Your guess is too high'
+    elsif guessed_num < winning_num
+      puts 'Your guess is too low'
+    else
+      puts you_won
+    end
   end
 end
 
