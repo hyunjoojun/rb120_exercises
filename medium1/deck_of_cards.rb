0b37fd0f4e2d838d071408b9937b7d65bec49876
Hyunjoo Jun <hjj102088@gmail.com>
2022-12-12 12:40:31 -0500

    Review

--- medium1/deck_of_cards.rb
+++ medium1/deck_of_cards.rb
@@ -1,29 +1,3 @@
-require 'pry'
-
-class Card
-  include Comparable
-  attr_reader :rank, :suit
-
-  VALUES = { 'Jack' => 11, 'Queen' => 12, 'King' => 13, 'Ace' => 14 }
-
-  def initialize(rank, suit)
-    @rank = rank
-    @suit = suit
-  end
-
-  def to_s
-    "#{rank} of #{suit}"
-  end
-
-  def value
-    VALUES.fetch(rank, rank)
-  end
-
-  def <=>(other_card)
-    value <=> other_card.value
-  end
-end
-
 class Deck
   RANKS = ((2..10).to_a + %w(Jack Queen King Ace)).freeze
   SUITS = %w(Hearts Clubs Diamonds Spades).freeze
@@ -48,12 +22,45 @@ class Deck
   end
 end
 
+class Card
+  include Comparable
+  attr_reader :rank, :suit
+
+  RANK_VALUES = { 'Jack' => 11, 'Queen' => 12, 'King' => 13, 'Ace' => 14 }
+  SUIT_VALUES = { "Diamonds" => 1, "Clubs" => 2, "Hearts" => 3, "Spades" => 4 }
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  def rank_value
+    RANK_VALUES.fetch(rank, rank)
+  end
+
+  def suit_value
+    SUIT_VALUES[suit]
+  end
+
+  def <=>(other)
+    if rank_value == other.rank_value
+      suit_value <=> other.suit_value
+    else
+      rank_value <=> other.rank_value
+    end
+  end
+end
+
 deck = Deck.new
 drawn = []
 52.times { drawn << deck.draw }
-drawn.count { |card| card.rank == 5 } == 4
-drawn.count { |card| card.suit == 'Hearts' } == 13
+p drawn.count { |card| card.rank == 5 } == 4
+p drawn.count { |card| card.suit == 'Hearts' } == 13
 
 drawn2 = []
 52.times { drawn2 << deck.draw }
-drawn != drawn2 # Almost always.
+p drawn != drawn2 # Almost always.
